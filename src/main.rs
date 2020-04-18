480d9148a3fc7f0a07fa54f7972e04cf1ac35c52
Tobin Yehle <tobinyehle@gmail.com>
2020-04-18 00:27:13 -0700

    basic led matrix control

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,128 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
 extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
+use feather_m0 as hal;
+
+use atsamd21g18a::{TC4, TC5};
 use cortex_m_rt::entry;
+use hal::clock::GenericClockController;
+use hal::gpio::*;
+use hal::pac::Peripherals;
+use hal::prelude::*;
+use hal::sercom::{SPIMaster4, Sercom4Pad0, Sercom4Pad2, Sercom4Pad3};
+use hal::timer::TimerCounter;
+
+use matrix_display::*;
+
+type SPI = SPIMaster4<Sercom4Pad0<Pa12<PfD>>, Sercom4Pad2<Pb10<PfD>>, Sercom4Pad3<Pb11<PfD>>>;
+type LEDPin = Pa17<Output<OpenDrain>>;
+
+/// Delay struct compatible with both the feather m0 timer and the LED Matrix
+#[derive(Clone, Copy)]
+struct DelayHertz(u32);
+impl From<DelayHertz> for hal::time::Hertz {
+    fn from(delay: DelayHertz) -> hal::time::Hertz {
+        hal::time::Hertz(delay.0)
+    }
+}
+
+impl core::ops::Shl<usize> for DelayHertz {
+    type Output = DelayHertz;
+    fn shl(self, amount: usize) -> DelayHertz {
+        DelayHertz(self.0 << amount)
+    }
+}
+
+/// Get the SPI bus setup
+fn setup() -> (
+    LEDPin,
+    TimerCounter<TC5>,
+    LEDArray<
+        Pa7<Output<OpenDrain>>,
+        Pa18<Output<OpenDrain>>,
+        Pa16<Output<OpenDrain>>,
+        TimerCounter<TC4>,
+        SPI,
+        Pa20<Output<OpenDrain>>,
+        Pa15<Output<OpenDrain>>,
+    >,
+) {
+    let mut peripherals = Peripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_external_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.PM,
+        &mut peripherals.SYSCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let mut pins = hal::Pins::new(peripherals.PORT);
+
+    // initialize the pins
+    let row_pins = (
+        pins.d9.into_open_drain_output(&mut pins.port),
+        pins.d10.into_open_drain_output(&mut pins.port),
+        pins.d11.into_open_drain_output(&mut pins.port),
+    );
+
+    let reg_pin = pins.d6.into_open_drain_output(&mut pins.port);
+    let output_disable = pins.d5.into_open_drain_output(&mut pins.port);
+
+    let mut red_led = pins.d13.into_open_drain_output(&mut pins.port);
+    red_led.set_low().unwrap();
+
+    // Setup the timer
+    let gclk0 = clocks.gclk0();
+    let tc45 = &clocks.tc4_tc5(&gclk0).unwrap();
+    let timer = hal::timer::TimerCounter::tc4_(tc45, peripherals.TC4, &mut peripherals.PM);
+
+    let tc5 = hal::timer::TimerCounter::tc5_(tc45, peripherals.TC5, &mut peripherals.PM);
+
+    // setup the SPI bus
+    let spi = hal::spi_master(
+        &mut clocks,
+        10.mhz(),
+        peripherals.SERCOM4,
+        &mut peripherals.PM,
+        pins.sck,
+        pins.mosi,
+        pins.miso,
+        &mut pins.port,
+    );
+
+    let mut array = LEDArray {
+        // array: image,
+        array: [[0; 16]; 8],
+        row_pins,
+        timer,
+        spi,
+        reg_pin,
+        output_disable,
+    };
+    // start the timer so we don't crash on the first scan
+    array.timer.start(1.mhz());
+
+    (red_led, tc5, array)
+}
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let (mut red_led, mut _timer, mut array) = setup();
 
+    array.array = [
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    ];
+
+    let base_scan_freq = DelayHertz(1000);
     loop {
-        // your code goes here
+        array.scan(base_scan_freq).unwrap_or(());
+        red_led.toggle();
     }
 }
